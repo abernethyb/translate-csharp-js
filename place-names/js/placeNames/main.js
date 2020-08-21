e00a9c2e595d4019cf944f2e7ae7aadcf0408198
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-08-21 14:59:01 -0500

    converted csharp to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,66 @@
+const places = ["Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+
+
 function main() {
-  // Put your code here
+
+  console.log("All place names");
+  places.forEach(place => {
+    console.log(place)
+  })
+  console.log("")
+  console.log(`"The" Place Names`)
+  const thePlaces = places.forEach(thePlace => {
+    thePlace.startsWith("The") && console.log(thePlace)
+  })
 
 }
-
 main();
+
+
+// //C# to be translated into JS
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
+
+// namespace PlaceNames
+// {
+//     // We write C# code in classes.
+//     class Program
+//     {
+//         // All C# applications start in the "Main" method.
+//         static void Main(string[] args)
+//         {
+//             // We must specify the Type of the "names" variable.
+//             //  It is a "List" that can only contain "strings".
+//             List<string> names = new List<string> {
+//                 "Nashville", "Hong Kong", "The back yard", "Earth",
+//                 "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+//             };
+
+//             // Print some output to the terminal (aka. "Console");
+//             Console.WriteLine("All Place Names");
+
+//             // Iterate over each "name" in the "names" list
+//             foreach (string name in names)
+//             {
+//                 Console.WriteLine(name);
+//             }
+
+//             // When you don't pass an argument to WriteLine, it simply outputs a blank line.
+//             //  Here we use that to add a little space around the output.
+//             Console.WriteLine();
+
+//             // Filter the list to create a new list that only has 
+//             //  place names that start with the word "The".
+//             List<string> theNames = names.Where(n => n.StartsWith("The")).ToList();
+
+//             Console.WriteLine("'The' Place Names");
+//             foreach (string name in theNames)
+//             {
+//                 Console.WriteLine(name);
+//             }
+//         }
+//     }
+// }
